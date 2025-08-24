dedf170dbcb6140c96488ff0c50c998941549f8e
Sangameshwar killare <sangameshkillare@gmail.com>
2025-08-24 08:40:36 +0530

    setup: venv, requirements, mysql config

--- backend/parking_project/settings/base.py
+++ backend/parking_project/settings/base.py
@@ -74,10 +74,17 @@ WSGI_APPLICATION = 'parking_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'parking_lelo_db',   # Your MySQL database name
+        'USER': 'root',              # Your MySQL username
+        'PASSWORD': 'root', # Your MySQL password
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
+
+
 
 
 # Password validation
